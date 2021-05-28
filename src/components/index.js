ecbd507c6037eb5b7cb3206f680b24de602f9c79
scy6500 <55119006+scy6500@users.noreply.github.com>
2021-05-29 00:07:05 +0900

    Delete src/components directory

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-
-import Sidebar from './Sidebar';
-import Example from './Example';
-import SearchBox from './SearchBox';
-import CrossModal from './CrossModal';
-
-export {Sidebar, Example, SearchBox, CrossModal};
