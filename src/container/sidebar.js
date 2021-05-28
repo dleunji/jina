4576b4455f583627e02847506da724c49c093912
Eunji Lee <eunji@comcom.ai>
2021-05-28 19:43:46 +0900

    remove console

--- src/container/sidebar.js
+++ src/container/sidebar.js
@@ -3,12 +3,8 @@ import {useSelector, useDispatch} from 'react-redux';
 
 const SidebarContainer = () => {
   const dispatch = useDispatch();
-  const jina = useSelector(state=>state.jina);
-  console.log(jina);
   const onChange = (id) => {
-    console.log("바뀌라");
     dispatch({type: 'MODE_CHANGE', id : id});
-    console.log(jina);
   }
   return (
   <Sidebar onChange={onChange}/>
