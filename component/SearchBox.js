fc8261a93b270d821575d5f98f46d14859f59e1d
scy6500 <55119006+scy6500@users.noreply.github.com>
2021-06-07 19:17:41 +0900

    add gif example

---
+++ component/SearchBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import ScriptTag from 'react-script-tag'
+import CustomScriptTag from './CustomScriptTag';
+
+const SearchBox = ({server}) =>{
+  const onLoad = () => {
+    console.log(server);
+    const jb = window.JinaBox;
+    jb.init(server,{ timeout:25000, top_k:16});
+  }
+  return(
+    <div>
+      <h2>Try it!</h2>
+      <jina-searchbar></jina-searchbar>
+      <CustomScriptTag src ="https://unpkg.com/jinabox" onLoad = {onLoad}/>
+    </div>
+  );
+}
+ 
+export default SearchBox;
---
+++ reducer/jina.js
@@ -0,0 +1,63 @@
+const infos = [
+  {
+    id: 'CrossModal',
+    title : 'Jina - Cross Modal Search System',
+    description: 'It allows the user to search for images given a caption description.',
+    server : 'https://master-crossmodal-dleunji.endpoint.ainize.ai/api/search',
+    githubLink : 'https://github.com/jina-ai/examples/tree/master/cross-modal-search',
+    ainizeLink: 'https://ainize.ai/dleunji/crossmodal',
+    inputType: "text",
+    input: "a dog running in the meadow",
+    output: "/images/CrossModalExample.png",
+  },
+  {
+    id: 'Poke',
+    title: 'Jina - Search Pokemon Images',
+    description: 'This example finds similar Pokémon by uploading a image.',
+    server : 'https://main-jina-pokemon-scy6500.endpoint.ainize.ai/api/search',
+    githubLink : 'https://github.com/jina-ai/examples/tree/master/pokedex-with-bit',
+    ainizeLink : 'https://ainize.ai/scy6500/jina-pokemon?branch=main',
+    inputType: "image",
+    input: "/images/PokeInput.jpeg",
+    output: "/images/PokeExample.png",
+  },
+  {
+    id: 'Object Search',
+    title: 'Jina - Object Search',
+    description: 'It allows the user to find the most relevant parent photo.',
+    server: 'https://master-object-search-dleunji.endpoint.ainize.ai/api/search',
+    githubLink : 'https://github.com/jina-ai/examples/tree/master/object-search',
+    ainizeLink: 'https://ainize.ai/dleunji/object-search?branch=master',
+    inputType: "image",
+    input: "/images/ObjectInput.png",
+    output: "/images/ObjectExample.png",
+
+  },
+  {
+    id: 'Gif Search',
+    title: 'Jina - Gif Search',
+    description: 'If you upload a gif file, it will find a similar gif file.',
+    server: 'https://main-jina-gif-search-scy6500.endpoint.ainize.ai/api/search',
+    githubLink : 'https://github.com/jina-ai/examples/tree/master/tumblr-gif-search',
+    ainizeLink: 'https://ainize.ai/scy6500/jina-gif-search?branch=main',
+    inputType: "image",
+    input: "/images/GifInput.png",
+    output: "/images/GifOutput.png",
+  },
+];
+
+const initialState = {
+  info : infos[0]
+};
+
+export const jina = (state = initialState, action) => {
+  switch (action.type) {
+    case 'MODE_CHANGE':
+      return {
+        ...state,
+        info : infos[action.id]
+      };
+    default:
+      return state;
+  };
+};
